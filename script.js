0367038cfd09ece43d2243ee0a721a535440ffbc
kumarjatinsheoran <jkumarsheoran2612@gmail.com>
2023-02-10 17:00:47 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    const welcome = document.getElementById('welcome');
+  
+    // 👇️ hides element (still takes up space on the page)
+    welcome.style.display = 'none';
+  }, 8000);
+  
