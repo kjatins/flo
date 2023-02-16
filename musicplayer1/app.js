6c82d752bfc0deb5860655278846b39cfcc18b02
kumarjatinsheoran <jkumarsheoran2612@gmail.com>
2023-02-16 20:47:18 +0530

    last commit

---
+++ musicplayer1/app.js
@@ -0,0 +1,122 @@
+console.log("Welcome to Lofi");
+
+//initialize the variables
+let songIndex = 0;
+let music = document.getElementById("audio");
+let backimg = document.querySelector(".back");
+let progressbar = document.getElementById("progressbar");
+let back = document.getElementById("pre");
+let playbtn = document.getElementById("play");
+let next = document.getElementById("next");
+let disk = document.querySelector(".disk");
+let songname = document.querySelector(".song-name");
+let artistname = document.querySelector(".artist-name");
+let currentTime = document.querySelector(".current-time");
+let songDuration = document.querySelector(".song-duration");
+let musicplayer = document.querySelector(".masterplayer");
+
+//songitems;
+
+
+//play/pause click:
+playbtn.addEventListener('click', () => {
+    if (music.paused || music.currentTime <= 0) {
+        music.play();
+        playbtn.classList.remove('masterplay');
+        playbtn.classList.add('pause');
+        disk.classList.add('play');
+    }
+    else {
+        music.pause();
+        playbtn.classList.remove('pause');
+        playbtn.classList.add('masterplay');
+        disk.classList.remove('play');
+    }
+})
+
+back.addEventListener('click', () => {
+    if (index <= 0) {
+        index = 0;;
+    }
+    else {
+        index -= 1;
+    }
+    music.src = `songs/music/${index + 1}.mp3`;
+    songname.innerHTML = songs[index].name;
+    artistname.innerHTML = songs[index].artist;
+    disk.style.backgroundImage = `url('${songs[index].cover}')`;
+    backimg.style.backgroundImage = `url('${songs[index].cover}')`;
+    music.currentTime = 0;
+    music.play();
+    playbtn.click();
+})
+
+next.addEventListener('click', () => {
+    if (index>=14) {
+        index = 0;
+    }
+    else {
+        index+=1;
+    }
+    music.src = `songs/music/${index + 1}.mp3`;
+    songname.innerHTML = songs[index].name;
+    artistname.innerHTML = songs[index].artist;
+    disk.style.backgroundImage = `url('${songs[index].cover}')`;
+    backimg.style.backgroundImage = `url('${songs[index].cover}')`;
+    music.currentTime = 0;
+    music.play();
+    playbtn.click();
+})
+
+///
+
+Array.from(document.getElementsByClassName('playbtn')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        musicplayer.style.display="contents";
+        index = parseInt(e.target.id);
+        music.src = `songs/music/${index + 1}.mp3`;
+        songname.innerHTML = songs[index].name;
+        artistname.innerHTML = songs[index].artist;
+        disk.style.backgroundImage = `url('${songs[index].cover}')`;
+        backimg.style.backgroundImage = `url('${songs[index].cover}')`;
+        music.currentTime = 0;
+        music.play();
+        playbtn.click(true)
+    })
+})
+
+
+//listen to event
+music.addEventListener('timeupdate', () => {
+    currentTime.innerHTML = '00:00';
+    setTimeout(() => {
+        progressbar.max = music.duration;
+        songDuration.innerHTML = formatTime(music.duration);
+
+    }, 100);
+
+
+    const formatTime = (time) => {
+        let min = Math.floor(time / 60);
+        if (min < 10) {
+            min = `0${min}`;
+        }
+        let sec = Math.floor(time % 60);
+        if (sec < 10) {
+            sec = `0${sec}`;
+        }
+        return `${min} :${sec}`;
+    }
+
+    //progress bar running
+    setInterval(() => {
+        progressbar.value = parseInt(music.currentTime);
+        currentTime.innerHTML = formatTime(music.currentTime);
+        if (Math.floor(music.currentTime) == Math.floor(progressbar.max)) {
+            next.click();
+        }
+    }, 100)
+})
+progressbar.addEventListener('change', () => {
+    music.currentTime = progressbar.value;
+})
